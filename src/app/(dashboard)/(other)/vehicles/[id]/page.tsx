a94795c3b7c86e9f233438ad3995e86be0ce0b15
Janith Madarasinghe <janithmadarasinghe@gmail.com>
2026-01-01 20:42:39 +0530

    alt

--- src/app/(dashboard)/(other)/vehicles/[id]/page.tsx
+++ src/app/(dashboard)/(other)/vehicles/[id]/page.tsx
@@ -1,12 +1,12 @@
 import { notFound } from "next/navigation"
 
-import { fetchVehicleDetails } from "@/app/(dashboard)/(other)/vehicles/_actions"
 import { requireAuth } from "@/lib/auth/get-session"
 
-import { VehicleHeader } from "../_components/vehicle-header"
-import { VehicleImages } from "../_components/vehicle-images"
+import { fetchVehicleDetails } from "./_actions"
 import VehicleBreadcrumbs from "./_components/vehicle-breadcrumbs"
 import { VehicleDetailsTabs } from "./_components/vehicle-details-tabs"
+import { VehicleHeader } from "./_components/vehicle-header"
+import { VehicleImages } from "./_components/vehicle-images"
 
 export default async function VehicleDetailPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
